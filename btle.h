0f7a05da230cbc2d28508fb92b3a7be26b6d33ff
Matt Schulte <schultetwin1@gmail.com>
2014-11-07 12:28:30 -0500

    [master] Added spacing to code
    This improves readability a little

--- btle.h
+++ btle.h
@@ -4,12 +4,16 @@ typedef struct _btle_t btle_t;
 
 btle_t* btle_new();
 void    btle_destroy(btle_t** self_p);
+
 int     btle_sock(btle_t* self);
 int     btle_dev_is_up(btle_t* self);
+
 int     btle_set_filter(btle_t* self);
+
 int     btle_set_adv_data(btle_t* self, void* buf, size_t len);
 int     btle_start_adv(btle_t* self);
 int     btle_stop_adv(btle_t* self);
+
 int     btle_start_scan(btle_t* self);
 int     btle_stop_scan(btle_t* self);
 #endif
